0a7458514fa0301d2fb98edb44c765f9f57cd6c5
AlexanderMatos1997 <47197201+AlexanderMatos1997@users.noreply.github.com>
2020-02-11 16:58:48 -0500

    GettterScript is implemented

---
+++ NewCapstone_prototype/Assets/Scripts/GetterActive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetterActive : MonoBehaviour
+{
+    GameObject[] a;
+
+    bool allTrue = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GameObject.FindGameObjectsWithTag("ScoreLight");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < a.Length; i++)
+        {
+            print(a[i].GetComponent<LightUpScore>().isActive);
+            if (!a[i].GetComponent<LightUpScore>().isActive)
+            {
+                allTrue = true;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ NewCapstone_prototype/Assets/Scripts/LightUpScore.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightUpScore : MonoBehaviour
+{
+    [SerializeField] private bool isActivated;
+
+    public bool isActive
+    {
+        get
+        {
+            return isActivated;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            if (!isActive)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(15f, 64f, 113f);
+                Debug.Log("Ball is detected");
+                isActivated = true;
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(0f, 128f, 255f);
+                isActivated = false;
+            }
+
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
