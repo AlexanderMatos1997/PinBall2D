690bf361706752bdfd4782ae1ecacb46625f5296
AlexanderMatos1997 <47197201+AlexanderMatos1997@users.noreply.github.com>
2020-02-28 18:08:13 -0500

    New Stage

--- NewCapstone_prototype/Assets/Scripts/Flipper_Code.cs
+++ NewCapstone_prototype/Assets/Scripts/Flipper_Code.cs
@@ -28,13 +28,13 @@ public class Flipper_Code : MonoBehaviour
     {
         if (Input.GetAxis(InputKey) == 1)
         {
-            hingeJM.motorSpeed = ActivePosition * 5;
+            hingeJM.motorSpeed = ActivePosition * 10;
             hingeJoint.motor = hingeJM;
             //Debug.Log("input is detected");
         }
         else
         {
-            hingeJM.motorSpeed = RestPosition * 5;
+            hingeJM.motorSpeed = RestPosition * 10;
             hingeJoint.motor = hingeJM;
             //Debug.Log("else is active");
         }
