04622a5e503ca0fb4bac8fc5e47912565c487dca
AlexanderMatos1997 <47197201+AlexanderMatos1997@users.noreply.github.com>
2020-03-17 16:52:33 -0400

    wav files inserted

--- NewCapstone_prototype/Assets/Scripts/Slingshot_Function.cs
+++ NewCapstone_prototype/Assets/Scripts/Slingshot_Function.cs
@@ -20,6 +20,7 @@ public class Slingshot_Function : MonoBehaviour
         
     }
 
+    /*
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Rigidbody2D rb2D = collision.gameObject.GetComponent<Rigidbody2D>();
@@ -45,28 +46,32 @@ public class Slingshot_Function : MonoBehaviour
         {
             Debug.Log("Ball wasn't fast enough");
         }
-    }
+    }*/
 
-        /*
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Rigidbody2D rb2D = collision.gameObject.GetComponent<Rigidbody2D>();
-
         Debug.Log("Rigidbody of ball is found");
-        Debug.Log("Ball's velocity is " + rb2D.angularVelocity);
+        Debug.Log("Ball's velocity is " + collision.attachedRigidbody.velocity);
 
-        if (rb2D != null && rb2D.angularVelocity > ForceMinimum)
+        if (rb2D != null && collision.attachedRigidbody.velocity.magnitude > ForceMinimum)
         {
-            if (rb2D.angularVelocity < relativeVelocityMax)
+            if (collision.attachedRigidbody.velocity.magnitude < relativeVelocityMax)
             {
                 Debug.Log("Ball is fast enough");
-                float pBall = rb2D.angularVelocity;
-                rb2D.velocity = new Vector2(rb2D.velocity.x * 2f, rb2D.velocity.y * 2f);
-                GameObject.AddForce(transform.forward); //* Slingshot_force * pBall);
-                Debug.Log("Ball's new velocity is " + rb2D.angularVelocity);
+                float pBall = collision.attachedRigidbody.velocity.magnitude;
+                rb2D.velocity = new Vector2(rb2D.velocity.x * 1.5f, rb2D.velocity.y * 1.5f);
+                rb2D.AddForce(transform.forward * Slingshot_force * pBall);
+                Debug.Log("Ball's new velocity is " + collision.attachedRigidbody.velocity.magnitude);
             }
-            else { Debug.Log("Ball was moving too fast"); }
+            else
+            {
+                Debug.Log("Ball was moving too fast");
+            }
         }
-        else { Debug.Log("Ball wasn't fast enough"); }
-    }*/
+        else
+        {
+            Debug.Log("Ball wasn't fast enough");
+        }
+    }
 }
